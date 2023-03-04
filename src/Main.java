2b5846db26e782b7994b8838b8a7928fcebed1b2
nimkoes <xxxelppa@gmail.com>
2023-03-04 19:11:11 +0900

    코드정리

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,15 @@ import semantle.Semantle;
 import wiktionary.WordExtractor;
 
 import java.io.IOException;
-import java.util.List;
 
 public class Main {
-    private static final int seq = 337;
+    private static final int SEQ = 337;
+    private static final int DELAY_MILLI = 30;
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println(seq + "회차 꼬맨틀 풀기 시작!!");
-
-        List<String> words = WordExtractor.extract();
-        String result = new Semantle(seq, 30).process(words);
-
-        System.out.println(seq + "회차 꼬맨틀 풀기 종료!!");
+        System.out.println(SEQ + "회차 꼬맨틀 풀기 시작!!");
+        String result = new Semantle(SEQ, DELAY_MILLI).process(WordExtractor.extract());
+        System.out.println(SEQ + "회차 꼬맨틀 풀기 종료!!");
         System.out.println("정답 = " + result);
     }
 }
